2857580b45280655762b3d7661ad36be44c7f274
Tom Eccles <t@freedommail.info>
2014-07-01 22:13:02 +0100

    dup() implemented
    took one fcntl and nothing else

--- dup.c
+++ dup.c
@@ -1,6 +1,8 @@
 #include <stdio.h>		// for printf and fflush
 #include <errno.h>		// for errno
 #include <stdlib.h>		// for exit
+#include <unistd.h>		// for fcntl
+#include <fcntl.h>		// for fcntl
 
 // implement int dup(int oldfd) -> returns new dup-ed file descriptor on succsess or -1 on error
 
@@ -17,9 +19,13 @@ void errExit(char* errMsg)
 	exit(EXIT_FAILURE);
 }
 
+int dup(int oldfd) {
+	return fcntl(oldfd, F_DUPFD, 0);	// duplicate oldfd to the first available fd number greater than or equal to 0
+}
+
 int main(int argc, char* argv[])
 {
-	
+	// test dup
 
 	exit(EXIT_SUCCESS);
 }
